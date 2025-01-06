a1d4d9a66798446a3d8002f382b2bf8a8ee3d3fb
Andrew Kane <andrew@ankane.org>
2025-01-05 21:07:22 -0800

    Fixed verify script [skip ci]

--- test/support/verify.py
+++ test/support/verify.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-arr = np.load("test/support/generated.npy")
-arrs = np.load("test/support/generated.npz")
-rank0 = np.load("test/support/generated_rank0.npy")
+arr = np.load("test/support/single.npy")
+arrs = np.load("test/support/multiple.npz")
+rank0 = np.load("test/support/rank0.npy")
 
 print(arr)
 print(arrs["x"])
